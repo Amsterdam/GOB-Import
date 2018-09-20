8fc9d72d081ef269a7fd7b6ac6b5bb67bd5bb48f
Jasper Jager <mail@jasperjager.com>
2018-09-20 15:28:24 +0200

    Add messagedriven_service to run docker
    A messagedriven_service listening to non existed messages was added to keep the docker container running. This way we can run the import jobs from jenkins.

--- src/gobimportclient/__main__.py
+++ src/gobimportclient/__main__.py
@@ -1,42 +1,65 @@
 """Main program structure for an import client
 
-Requires a dataset description-file to run:
+The import client subscribes to the workflow queue if started without arguments.
+
+Requires a dataset description-file to run an import:
 
      python -m gobimportclient example/meetbouten.json
 
 :return: None
+
 """
 import argparse
 
+from gobcore.message_broker.messagedriven_service import messagedriven_service
 from gobcore.log import get_logger
 from gobcore.model import GOBModel
 
 from gobimportclient.import_client import ImportClient
 from gobimportclient.mapping import get_mapping
 
+# If we have args we are expected to process the datafile
+# TODO: Listen to the correct events in the queue, instead of manual trigger
+
 logger = get_logger(name=__name__)
 
 # Parse the arguments to get the import directory, e.g. /download
 parser = argparse.ArgumentParser(description='Import datasource.')
 parser.add_argument('datasource_description',
-                    nargs='+', type=str,
+                    nargs='*', type=str,
                     help='the name of the datasource description (`meetbout`')
 args = parser.parse_args()
 
-gob_model = GOBModel()
-for input_name in args.datasource_description:
+if len(args.datasource_description) > 0:
+    gob_model = GOBModel()
 
-    logger.info("Reading dataset description")
+    for input_name in args.datasource_description:
 
-    logger.info(f"Import dataset {input_name} starts")
+        logger.info("Reading dataset description")
 
-    import_client = ImportClient(dataset=get_mapping(input_name))
+        logger.info(f"Import dataset {input_name} starts")
 
-    try:
-        import_client.connect()
-        import_client.read()
-        import_client.convert()
-        import_client.publish()
-        logger.info(f"Import dataset {input_name} ended")
-    except Exception as e:
-        logger.error(f"Import dataset {input_name} failed: {str(e)}")
+        import_client = ImportClient(dataset=get_mapping(input_name))
+
+        try:
+            import_client.connect()
+            import_client.read()
+            import_client.convert()
+            import_client.publish()
+            logger.info(f"Import dataset {input_name} ended")
+        except Exception as e:
+            logger.error(f"Import dataset {input_name} failed: {str(e)}")
+
+else:
+    # Start message driven service to keep the docker alive
+    SERVICEDEFINITION = {
+        'dataimport.proposal': {
+            'queue': "gob.workflow.proposal",
+            # for now only pass through the message-content:
+            'handler': lambda msg: msg,
+            'report_back': 'dataimport.request',
+            'report_queue': 'gob.workflow.request'
+        },
+    }
+
+    messagedriven_service(SERVICEDEFINITION)
