f8695c38b4d2b1e68977ce339ec2a42f19fd7d3c
vegter <amsjurien@gmail.com>
2018-11-01 12:18:29 +0100

    Rename gobimportclient to gobimport

---
+++ src/gobimport/connector/__init__.py
@@ -0,0 +1,5 @@
+from gobimport.connector.database import connect_to_database
+from gobimport.connector.objectstore import connect_to_objectstore
+from gobimport.connector.file import connect_to_file
+
+__all__ = ['connect_to_database', 'connect_to_objectstore', 'connect_to_file']
---
+++ src/gobimport/reader/__init__.py
@@ -0,0 +1,5 @@
+from gobimport.reader.database import read_from_database
+from gobimport.reader.objectstore import read_from_objectstore
+from gobimport.reader.file import read_from_file
+
+__all__ = ['read_from_database', 'read_from_objectstore', 'read_from_file']
