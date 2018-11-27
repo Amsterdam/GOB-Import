5a813a225a7ffd699ef1cff8170a03f5ec4ad09e
vegter <amsjurien@gmail.com>
2018-11-27 10:58:49 +0100

    DIVA buurten import
    Add DGDialog and DIVA as import sources.
    Update validation logic for entities with state

--- src/gobimport/connector/config.py
+++ src/gobimport/connector/config.py
@@ -8,19 +8,26 @@ DATABASE_CONFIGS = {
         'host': os.getenv("DBIMBP01_DATABASE_HOST", "hostname"),
         'port': os.getenv("DBIMBP01_DATABASE_PORT", 1521),
         'database': 'DBIMBP01'
+    },
+    'DGDialog': {
+        'drivername': 'oracle+cx_oracle',
+        'username': os.getenv("BINBGT01_DATABASE_USER", "gob"),
+        'password': os.getenv("BINBGT01_DATABASE_PASSWORD", "insecure"),
+        'host': os.getenv("BINBGT01_DATABASE_HOST", "hostname"),
+        'port': os.getenv("BINBGT01_DATABASE_PORT", 1521),
+        'database': 'binbgt11.amsterdam.nl'
+    },
+    'DIVA': {
+        'drivername': 'oracle+cx_oracle',
+        'username': os.getenv("DBIGMA01_DATABASE_USER", "gob"),
+        'password': os.getenv("DBIGMA01_DATABASE_PASSWORD", "insecure"),
+        'host': os.getenv("DBIGMA01_DATABASE_HOST", "hostname"),
+        'port': os.getenv("DBIGMA01_DATABASE_PORT", 1521),
+        'database': 'DBIGMA01'
     }
 }
 
 OBJECTSTORE_CONFIGS = {
-    'Milieuthemas': {
-        "VERSION": '2.0',
-        "AUTHURL": 'https://identity.stack.cloudvps.com/v2.0',
-        "TENANT_NAME": os.getenv("MILIEUTHEMAS_OBJECTSTORE_TENANT_NAME"),
-        "TENANT_ID": os.getenv("MILIEUTHEMAS_OBJECTSTORE_TENANT_ID"),
-        "USER": os.getenv("MILIEUTHEMAS_OBJECTSTORE_USER"),
-        "PASSWORD": os.getenv("MILIEUTHEMAS_OBJECTSTORE_PASSWORD"),
-        "REGION_NAME": 'NL'
-    },
     'Basisinformatie': {
         "VERSION": '2.0',
         "AUTHURL": 'https://identity.stack.cloudvps.com/v2.0',
