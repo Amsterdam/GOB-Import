d1af0c7f6dd81d0958203ce0c25880c4975e85e4
vegter <amsjurien@gmail.com>
2018-12-05 08:33:04 +0100

    Test improvements for robustness message handler
    Add test with failing message handler

--- src/tests/test_injections.py
+++ src/tests/test_injections.py
@@ -109,7 +109,7 @@ class TestInjections(unittest.TestCase):
             {
                 "id": 1,
                 "field1": "0",
-                "field2": 0,
+                "field2": 2,
                 "field3": "x"
             }
         ]
@@ -135,55 +135,9 @@ class TestInjections(unittest.TestCase):
             {
                 "id": 1,
                 "field1": "aap",
-                "field2": 10,
+                "field2": 12,
                 "field3": "x"
             }
         ]
         inject(inject_spec, data)
         self.assertEqual(data, expect)
-
-
-    # def test_validat_data(self):
-    #     validator = Validator(self.mock_import_client, 'meetbouten', self.valid_meetbouten, 'identificatie')
-    #     validator.validate()
-    #
-    # def test_duplicate_primary_key(self):
-    #     self.valid_meetbouten.append(self.valid_meetbouten[0])
-    #     validator = Validator(self.mock_import_client, 'meetbouten', self.valid_meetbouten, 'identificatie')
-    #
-    #     with self.assertRaises(GOBException):
-    #         validator.validate()
-    #
-    # def test_invalid_data(self):
-    #     validator = Validator(self.mock_import_client, 'meetbouten', self.invalid_meetbouten, 'identificatie')
-    #     validator.validate()
-    #
-    #     # Make sure the statusid has been listed as invalid
-    #     self.assertEqual(validator.collection_qa['num_invalid_status_id'], 1)
-    #
-    # def test_fatal_value(self):
-    #     validator = Validator(self.mock_import_client, 'meetbouten', self.fatal_meetbouten, 'identificatie')
-    #
-    #     with self.assertRaises(GOBException):
-    #         validator.validate()
-    #
-    #     # Make sure the identificatie has been listed as invalid
-    #     self.assertEqual(validator.collection_qa['num_invalid_identificatie'], 1)
-    #
-    # def test_missing_warning_data(self):
-    #     missing_attr_meetbouten = self.valid_meetbouten[0].pop('status_id')
-    #     validator = Validator(self.mock_import_client, 'meetbouten', self.valid_meetbouten, 'identificatie')
-    #     validator.validate()
-    #
-    #     # Make sure the publiceerbaar has been listed as invalid
-    #     self.assertEqual(validator.collection_qa['num_invalid_status_id'], 1)
-    #
-    # def test_missing_fatal_data(self):
-    #     missing_attr_meetbouten = self.valid_meetbouten[0].pop('publiceerbaar')
-    #     validator = Validator(self.mock_import_client, 'meetbouten', self.valid_meetbouten, 'identificatie')
-    #
-    #     with self.assertRaises(GOBException):
-    #         validator.validate()
-    #
-    #     # Make sure the publiceerbaar has been listed as invalid
-    #     self.assertEqual(validator.collection_qa['num_invalid_publiceerbaar'], 1)
