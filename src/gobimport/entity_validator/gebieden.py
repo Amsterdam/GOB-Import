e381a420c7603da21668b5259df5d4f0f96c13dc
Jasper Jager <mail@jasperjager.com>
2019-03-05 16:13:51 +0100

    Convert to python objects instead of GOBType
    Converting to GOBType uses a lot of memory so for now we only convert to GOBType to validate input. We store the python value of each attribute to reduce memory usage

--- src/gobimport/entity_validator/gebieden.py
+++ src/gobimport/entity_validator/gebieden.py
@@ -23,15 +23,14 @@ def _validate_bouwblokken(entities, source_id):
 
     for entity in entities:
         # begin_geldigheid can not be in the future
-        # to_db is used to work around the typesystem: https://github.com/Amsterdam/GOB-Core/issues/127
         identificatie = str(entity[source_id])
-        if entity['begin_geldigheid'].to_db > datetime.datetime.utcnow():
+        if entity['begin_geldigheid'] > datetime.datetime.utcnow():
             msg = "begin_geldigheid can not be in the future"
             extra_data = {
                 'id': msg,
                 'data': {
                     'identificatie': identificatie,
-                    'begin_geldigheid': entity['begin_geldigheid'],
+                    'begin_geldigheid': entity['begin_geldigheid'].isoformat(),
                 }
             }
             logger.error(msg, extra_data)
@@ -55,15 +54,14 @@ def _validate_buurten(entities, source_id):
 
     for entity in entities:
         # get eind_geldigheid or use current date
-        # to_db is used to work around the typesystem: https://github.com/Amsterdam/GOB-Core/issues/127
-        eind_geldigheid = entity['eind_geldigheid'].to_db if entity['eind_geldigheid'].to_db \
+        eind_geldigheid = entity['eind_geldigheid'] if entity['eind_geldigheid'] \
             else datetime.datetime.utcnow()
-        documentdatum = entity['documentdatum'].to_db
+        documentdatum = entity['documentdatum']
         # documentdatum should not be after eind_geldigheid
         if documentdatum and documentdatum > eind_geldigheid:
             log_date_comparison(entity, 'documentdatum', 'eind_geldigheid', source_id)
 
-        registratiedatum = entity['registratiedatum'].to_db
+        registratiedatum = entity['registratiedatum']
         # registratiedatum should not be after eind_geldigheid
         if registratiedatum and registratiedatum > eind_geldigheid:
             log_date_comparison(entity, 'registratiedatum', 'eind_geldigheid', source_id)
@@ -89,8 +87,8 @@ def log_date_comparison(entity, date_field, compare_date_field, source_id):
         'id': msg,
         'data': {
             'identificatie': identificatie,
-            '{date_field}': entity[date_field],
-            '{compare_date_field}': entity[compare_date_field],
+            '{date_field}': entity[date_field].isoformat(),
+            '{compare_date_field}': entity[compare_date_field].isoformat(),
         }
     }
     logger.warning(msg, extra_data)
