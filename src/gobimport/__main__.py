0ffc8d6cb60267662c3bc8e600ec7b882df38aef
Jaap Koetsier <jaap@chariot-it.nl>
2019-05-02 11:15:34 +0200

    Change import argument passing

--- src/gobimport/__main__.py
+++ src/gobimport/__main__.py
@@ -10,31 +10,61 @@ from gobimport.import_client import ImportClient
 from gobimport.mapping import get_dataset_file_location, get_mapping
 
 
+def _extract_dataset_variable(dataset):
+    """Returns dataset file from dataset variable. If dataset is a string it is assumed to be a file path. If dataset
+    is a dict we perform a lookup to get the correct file path.
+
+    :param dataset:
+    :return:
+    """
+    if isinstance(dataset, str):
+        return dataset
+    elif isinstance(dataset, dict):
+        keys = ['catalogue', 'collection', 'application']
+        if not all([key in dataset for key in keys]):
+            raise GOBException(f"Missing dataset keys. Expected keys: {','.join(keys)}")
+        return get_dataset_file_location(dataset['catalogue'], dataset['collection'], dataset['application'])
+    else:
+        raise GOBException("Dataset of invalid type. Expecting str or dict")
+
+
 def extract_dataset_from_msg(msg):
     """Returns location of dataset file from msg.
 
-    Dataset files can be identified by the file location ('dataset_file') or dataset identifier ('dataset') in the root
-    of the message or in the 'contents' part of the message:
+    The dataset to import should be defined either in the root of the msg or in the 'contents' part of the msg.
+    The dataset can be defined as string (the dataset location) or as dictionary (with catalogue, collection and
+    application as keys), in which case we look up the correct file in the available datasets.
 
-    message = { "dataset_file": "data/somedataset.json" }
+    message = { "dataset": "data/somedataset.json" }
     OR
-    message = { "dataset": "some:dataset:identifier" }
+    message = {
+    "dataset": {
+       "catalogue": "some catalogue",
+       "collection": "the collection",
+       "application": "the application"
+        }
+    }
     OR
-    message = { "contents": { "dataset_file": "data/somedataset.json" } }
+    message = { "contents": { "dataset": "data/somedataset.json" } }
     OR
-    message = { "contents": { "dataset": "some:dataset:identifier" } }
+    message = {
+    "contents": {
+        "dataset": {
+            "catalogue": "some catalogue",
+            "collection": "the collection",
+            "application": "the application"
+            }
+        }
+    }
 
     :param msg:
     :return:
     """
-    if 'dataset_file' in msg:
-        return msg['dataset_file']
-    elif msg.get('contents') and msg['contents'].get('dataset_file'):
-        return msg['contents']['dataset_file']
-    elif 'dataset' in msg:
-        return get_dataset_file_location(msg['dataset'])
+
+    if 'dataset' in msg:
+        return _extract_dataset_variable(msg['dataset'])
     elif msg.get('contents') and msg['contents'].get('dataset'):
-        return get_dataset_file_location(msg['contents']['dataset'])
+        return _extract_dataset_variable(msg['contents']['dataset'])
     else:
         raise GOBException('Missing dataset file')
 
