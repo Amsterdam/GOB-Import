0d966055350ee4092d326f1c59322b545d1afbfa
Jasper Jager <mail@jasperjager.com>
2019-07-30 16:45:52 +0200

    Import beperkingen and relations

--- src/gobimport/enricher/wkpb.py
+++ src/gobimport/enricher/wkpb.py
@@ -18,13 +18,8 @@ class WKPBEnricher(Enricher):
         self.multiple_values_logged = False
 
         super().__init__(app_name, catalogue_name, entity_name, methods={
-            "beperkingen": self.enrich_beperkingen,
             "dossiers": self.enrich_dossier,
         })
-
-    def enrich_beperkingen(self, beperking):
-        if beperking['belast_brk_kadastraal_object']:
-            beperking['belast_brk_kadastraal_object'] = beperking['belast_brk_kadastraal_object'].split(";")
 
     def enrich_dossier(self, dossier):
         dossier['heeft_wkpb_brondocument'] = dossier['heeft_wkpb_brondocument'].split(";")
