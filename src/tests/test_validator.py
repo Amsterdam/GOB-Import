1935b83557500deb2dba6f4343128892eb603cfc
Jasper Jager <mail@jasperjager.com>
2019-10-21 16:23:06 +0200

    Use volgnummer from column on GGW GGP
    Changing volgnummer on GGW/GGP resulted in some findings on entity validation and convertion. The code assumed an entity with states always has a column named ‘volgnummer’ while this should be configurable in the import mapping. Fox example the column in the GGW/GGP files was named ‘VOLGNUMMER’. Changes were made to core and validator to make this possible.

--- src/tests/test_validator.py
+++ src/tests/test_validator.py
@@ -23,27 +23,95 @@ class TestValidator(unittest.TestCase):
         self.nopubliceerbaar_meetbouten = fixtures.get_nopubliceerbaar_meetbouten()
         self.nullpubliceerbaar_meetbouten = fixtures.get_nullpubliceerbaar_meetbouten()
 
+        self.mock_input_spec = {
+            'catalogue': 'meetbouten',
+            'entity': 'meetbouten',
+            'source': {
+                'entity_id': 'identificatie'
+            }
+        }
+
         self.mock_import_client = mock.MagicMock(spec=ImportClient)
         self.mock_import_client.source = {
             'name': 'test'
         }
 
     def test_validat_data(self):
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
         for entity in self.valid_meetbouten:
             validator.validate(entity)
 
     def test_duplicate_primary_key(self):
         self.valid_meetbouten.append(self.valid_meetbouten[0])
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
 
         with self.assertRaises(GOBException):
             for entity in self.valid_meetbouten:
                 validator.validate(entity)
             validator.result()
 
+    def test_valid_primary_key_with_states(self):
+        mock_input_spec = {
+            'catalogue': 'bag',
+            'entity': 'woonplaatsen',
+            'source': {
+                'entity_id': 'identificatie'
+            }
+        }
+
+        valid_entity = fixtures.get_valid_entity_with_state()
+        validator = Validator('source_app', 'woonplaatsen', 'identificatie', mock_input_spec)
+
+        for entity in valid_entity:
+            validator.validate(entity)
+        validator.result()
+
+    def test_valid_primary_key_with_states_other_seqnr(self):
+        mock_input_spec = {
+            'catalogue': 'bag',
+            'entity': 'woonplaatsen',
+            'source': {
+                'entity_id': 'identificatie'
+            },
+            'gob_mapping': {
+                'volgnummer': {
+                    'source_mapping': 'nummervolg'
+                }
+            }
+        }
+
+        valid_entity = [{'identificatie': '1234', 'nummervolg': '1'}]
+        validator = Validator('source_app', 'woonplaatsen', 'identificatie', mock_input_spec)
+
+        for entity in valid_entity:
+            validator.validate(entity)
+        validator.result()
+
+    def test_duplicate_primary_key_with_states_other_seqnr(self):
+        mock_input_spec = {
+            'catalogue': 'bag',
+            'entity': 'woonplaatsen',
+            'source': {
+                'entity_id': 'identificatie'
+            },
+            'gob_mapping': {
+                'volgnummer': {
+                    'source_mapping': 'nummervolg'
+                }
+            }
+        }
+
+        valid_entity = [{'identificatie': '1234', 'nummervolg': '1'}]
+        valid_entity.append(valid_entity[0])
+        validator = Validator('source_app', 'woonplaatsen', 'identificatie', mock_input_spec)
+
+        with self.assertRaises(GOBException):
+            for entity in valid_entity:
+                validator.validate(entity)
+            validator.result()
+
     def test_invalid_data(self):
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
         for entity in self.invalid_meetbouten:
             validator.validate(entity)
 
@@ -51,7 +119,7 @@ class TestValidator(unittest.TestCase):
         self.assertEqual(validator.collection_qa['num_invalid_status_id'], 1)
 
     def test_fatal_value(self):
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
 
         with self.assertRaises(GOBException):
             for entity in self.fatal_meetbouten:
@@ -63,7 +131,7 @@ class TestValidator(unittest.TestCase):
 
     def test_missing_warning_data(self):
         missing_attr_meetbouten = self.valid_meetbouten[0].pop('status_id')
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
 
         for entity in self.valid_meetbouten:
             validator.validate(entity)
@@ -73,7 +141,7 @@ class TestValidator(unittest.TestCase):
 
     def test_missing_fatal_data(self):
         missing_attr_meetbouten = self.valid_meetbouten[0].pop('publiceerbaar')
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
 
         for entity in self.valid_meetbouten:
             validator.validate(entity)
@@ -82,7 +150,7 @@ class TestValidator(unittest.TestCase):
         self.assertEqual(validator.collection_qa['num_invalid_publiceerbaar'], 1)   # Warning
 
     def test_nopubliceerbaar(self):
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
 
         for entity in self.nopubliceerbaar_meetbouten:
             validator.validate(entity)
@@ -91,7 +159,7 @@ class TestValidator(unittest.TestCase):
         self.assertEqual(validator.collection_qa['num_invalid_publiceerbaar'], 1)   # Warning
 
     def test_nullpubliceerbaar(self):
-        validator = Validator('source_app', 'meetbouten', 'identificatie')
+        validator = Validator('source_app', 'meetbouten', 'identificatie', self.mock_input_spec)
 
         for entity in self.nullpubliceerbaar_meetbouten:
             validator.validate(entity)
