5d83b6dfc6e4f7ad9ddb0910afb18bae3dde6a0b
vegter <amsjurien@gmail.com>
2020-02-27 15:12:26 +0100

    Identify required changes

--- src/gobimport/entity_validator/state.py
+++ src/gobimport/entity_validator/state.py
@@ -42,9 +42,9 @@ class StateValidator:
         self._validate_begin_geldigheid(entity)
 
         # volgnummer should a positive number and unique in the collection
-        volgnummer = str(entity['volgnummer'])
+        volgnummer = str(entity['volgnummer'])  NOT NEEDED
         identificatie = str(entity[self.source_id])
-        if int(volgnummer) < 1 or volgnummer in self.volgnummers[identificatie]:
+        if int(volgnummer) < 1 or volgnummer in self.volgnummers[identificatie]:  NOT NEEDED
             msg = "volgnummer should be a positive number and unique in the collection"
             extra_data = {
                 'id': msg,
