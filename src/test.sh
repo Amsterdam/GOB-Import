ecbcdaaf33b3a14a6b36626fccf0bbd2867f042e
vegter <amsjurien@gmail.com>
2020-05-18 14:32:42 +0200

    Extract file value catches GOB type exceptions
    If a type conversion fails a data error is reported
    A None value is returned in order to not block further Import processing
    
    The data error will however block any further GOB processing

--- src/test.sh
+++ src/test.sh
@@ -13,4 +13,4 @@ echo "Running unit tests"
 pytest tests/
 
 echo "Running coverage tests"
-pytest --cov=gobimport --cov-report html --cov-fail-under=88
+pytest --cov=gobimport --cov-report html --cov-fail-under=89
