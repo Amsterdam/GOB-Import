22bcbaf8bde332783fa92d43d4f4ecd31ff96661
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-01-11 14:55:41 +0100

    Remove `brk` and `wkpb` enrichers

--- src/gobimport/enricher/__init__.py
+++ src/gobimport/enricher/__init__.py
@@ -5,8 +5,6 @@ This enricher calls some specific functions for collections to add missing value
 from gobimport.enricher.meetbouten import MeetboutenEnricher
 from gobimport.enricher.gebieden import GebiedenEnricher
 from gobimport.enricher.bag import BAGEnricher
-from gobimport.enricher.brk import BRKEnricher
-from gobimport.enricher.wkpb import WKPBEnricher
 from gobimport.enricher.test_catalogue import TstCatalogueEnricher
 
 
@@ -20,8 +18,7 @@ class BaseEnricher:
         :param entity_name:
         """
         self.enrichers = []
-        for CatalogueEnricher in [GebiedenEnricher, MeetboutenEnricher, BAGEnricher, BRKEnricher, WKPBEnricher,
-                                  TstCatalogueEnricher]:
+        for CatalogueEnricher in [GebiedenEnricher, MeetboutenEnricher, BAGEnricher, TstCatalogueEnricher]:
             if CatalogueEnricher.enriches(app_name, catalog_name, entity_name):
                 self.enrichers.append(CatalogueEnricher(app_name, catalog_name, entity_name))
 
