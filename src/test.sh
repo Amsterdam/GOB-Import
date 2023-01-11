a322a81835b08227165f2aaf4547df29cdf46b82
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-01-11 15:30:44 +0100

    Allow per file enable checks

--- src/test.sh
+++ src/test.sh
@@ -4,8 +4,32 @@ set -u
 
 export COVERAGE_FILE="/tmp/.coverage"
 
+# Uncomment files to pass through checks
+FILES=(
+#  "gobimport/enricher/bag.py"
+#  "gobimport/enricher/meetbouten.py"
+#  "gobimport/enricher/test_catalogue.py"
+#  "gobimport/enricher/__init__.py"
+#  "gobimport/enricher/enricher.py"
+#  "gobimport/enricher/gebieden.py"
+#  "gobimport/entity_validator/bag.py"
+#  "gobimport/entity_validator/__init__.py"
+#  "gobimport/entity_validator/gebieden.py"
+#  "gobimport/entity_validator/state.py"
+#  "gobimport/validator/config.py"
+#  "gobimport/validator/__init__.py"
+#  "gobimport/converter.py"
+  "gobimport/__init__.py"
+#  "gobimport/import_client.py"
+#  "gobimport/reader.py"
+#  "gobimport/utils.py"
+#  "gobimport/injections.py"
+#  "gobimport/__main__.py"
+#  "gobimport/merger.py"
+)
+
 echo "Running mypy"
-mypy gobimport
+mypy "${FILES[@]}"
 
 echo "Running unit tests"
 coverage run -m pytest
@@ -14,12 +38,12 @@ echo "Reporting coverage"
 coverage report --fail-under=96
 
 echo "Check if black finds no potential reformat fixes"
-black --check gobimport
+black --verbose --check "${FILES[@]}"
 
 echo "Check for potential import sort"
-isort --check --diff gobimport
+isort --check --diff "${FILES[@]}"
 
 echo "Running flake8"
-flake8 gobimport
+flake8 "${FILES[@]}"
 
 echo "Checks complete"
